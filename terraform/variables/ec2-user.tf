fad6e0088e94a10cb969d61e60f68a658d70d27d
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-03 12:31:38 +0530

    Create ec2-user.tf

---
+++ terraform/variables/ec2-user.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "MyDemoEC2Instance" {
+  ami                    = var.ami_id # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type          = var.instance_type
+  key_name               = "bcreddydevops-KP"
+  vpc_security_group_ids = [aws_security_group.BCReddySG_Demo.id]
+
+
+  tags = var.ec2_tags
+
+}
+
+
+resource "aws_security_group" "BCReddySG_Demo" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  ingress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+}
