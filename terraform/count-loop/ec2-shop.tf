2f382a49f4c56e07dd7decb470d4077d7cccc88e
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-12 09:43:30 +0530

    ec2-shopapp.tf

---
+++ terraform/count-loop/ec2-shop.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "bcreddy-ec2" {
+  count                  = length(var.instance_names)
+  ami                    = "ami-0c3389a4fa5bddaad" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type          = "t3.micro"
+  key_name               = "bcreddydevops-KP"
+  vpc_security_group_ids = [aws_security_group.bcreddy-sg.id]
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+
+}
+
+resource "aws_security_group" "bcreddy-sg" {
+  name        = "fruitshopsg"
+  description = "Security group for BCReddy Demo"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "fruitshop-sg-allow-all"
+  }
+}
