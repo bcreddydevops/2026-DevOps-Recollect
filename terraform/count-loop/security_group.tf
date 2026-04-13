718c72e099de5bcc945f9246ae0add459e3451bd
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-13 21:35:57 +0530

    Create security_group.tf

---
+++ terraform/count-loop/security_group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "main_sg" {
+  name        = "fruitshop-sg"
+  description = "Demo SG - Allow All (Not for Production)"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "fruitshop-sg"
+  }
+}
