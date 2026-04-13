489eb859ff75517fafe0fa018bde0cc2eca8544c
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-13 21:44:25 +0530

    Add dynamic security group configuration

---
+++ terraform/dynamic_blocks/sg_list.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "dynamic_sg" {
+  name        = "dynamic-sg-allow-all"
+  description = "Security group for BCReddy Demo"
+ # here ingress is going to be special variable
+  dynamic "ingress" {
+    for_each = var.ingress_ports_list
+    content {
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "fruitshop-sg-allow-all"
+  }
+}
