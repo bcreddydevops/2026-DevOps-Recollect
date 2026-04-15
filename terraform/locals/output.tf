abf160b943ed6cebd06dd25e3ec502039ddd6eb8
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-15 20:32:58 +0530

    Create output.tf

---
+++ terraform/locals/output.tf
@@ -0,0 +1,15 @@
+output "name_prefix" {
+  value = local.name_prefix
+}
+
+output "instance_type" {
+  value = local.instance_type
+}
+
+output "instance_names" {
+  value = local.instance_full_names
+}
+
+output "tags" {
+  value = local.final_tags
+}
