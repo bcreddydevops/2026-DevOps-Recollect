05dcf4c61ee55a29d7058610451628dbc913419e
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-17 18:31:25 +0530

    Create ec2.tf

---
+++ terraform/remote-state/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "bcreddy-ec2" {
+  ami                    = "ami-0c3389a4fa5bddaad" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type          = "t3.small"
+  key_name               = "bcreddydevops-KP"
+  vpc_security_group_ids = [aws_security_group.bcreddy-sg.id]
+
+  tags = {
+    Name = "fruitshopapp"
+  }
+
+}
+
+resource "aws_security_group" "bcreddy-sg" {
+  name        = "fruitshopsg"
+  description = "Security group for BCReddy Demo"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "fruitshop-sg-allow-all"
+  }
+}
