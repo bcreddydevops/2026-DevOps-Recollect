2836db044e057b66fe65b6d99005f59e0d960e0a
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-17 18:30:53 +0530

    Add Terraform provider and backend configuration

---
+++ terraform/remote-state/provider.tf
@@ -0,0 +1,22 @@
+#provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "remote-state-bcreddy2026"
+    key     = "remote-state.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
