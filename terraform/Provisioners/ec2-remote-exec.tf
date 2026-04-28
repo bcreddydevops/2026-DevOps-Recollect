0e9a24443230d2dc7c7b3a2a4e767275d0c27d25
bcreddydevops <93207503+bcreddydevops@users.noreply.github.com>
2026-04-28 20:08:14 +0530

    Update ec2-remote-exec.tf

--- terraform/Provisioners/ec2-remote-exec.tf
+++ terraform/Provisioners/ec2-remote-exec.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "remote_demo" {
-  ami                    = "ami-0c3389a4fa5bddaad"
-  instance_type          = "t3.micro"
-  key_name               = "bcreddydevops-KP"
-  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  ami                         = "ami-0c3389a4fa5bddaad"
+  instance_type               = "t3.micro"
+  key_name                    = "bcreddydevops-KP"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.demo_sg.id]
 
   tags = {
     Name = "Remote-Exec-Demo"
@@ -13,12 +14,13 @@ resource "aws_instance" "remote_demo" {
     user        = "ec2-user"
     private_key = file("bcreddydevops-KP.pem")
     host        = self.public_ip
+    timeout     = "3m"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install nginx1 -y",
+      "sudo dnf update -y",
+      "sudo dnf install nginx -y",
       "sudo systemctl start nginx",
       "sudo systemctl enable nginx",
       "echo '<h1>Welcome to BCReddyDevOps</h1><h2>Terraform Remote-Exec Provisioner Demo</h2>' | sudo tee /usr/share/nginx/html/index.html"
